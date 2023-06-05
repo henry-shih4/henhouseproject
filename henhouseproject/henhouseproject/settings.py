225c956ff660b62e3547f9c1fa4479dae571b591
Henry Shih <100496651+henry-shih4@users.noreply.github.com>
2023-06-05 12:15:56 -0400

    static files

--- henhouseproject/henhouseproject/settings.py
+++ henhouseproject/henhouseproject/settings.py
@@ -178,5 +178,5 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
