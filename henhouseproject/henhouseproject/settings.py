b36679dfdfcf4c617d3649c2a945e8cc5f84569d
Henry Shih <100496651+henry-shih4@users.noreply.github.com>
2023-06-08 12:32:10 -0400

    static files append media root

--- henhouseproject/henhouseproject/settings.py
+++ henhouseproject/henhouseproject/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
@@ -160,6 +160,9 @@ STATICFILES_DIRS = [
 ##user uploaded content
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+
+if not DEBUG: STATICFILES_DIRS.append(MEDIA_ROOT)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
