d5d91397c548410951fc4322e4e0700d151d46a0
Henry Shih <100496651+henry-shih4@users.noreply.github.com>
2023-06-15 10:37:09 -0400

    taking out vercel json

--- henhouseproject/henhouseproject/settings.py
+++ henhouseproject/henhouseproject/settings.py
@@ -192,7 +192,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
